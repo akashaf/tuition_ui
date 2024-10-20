fa82b3bf1af13433c85e2bcad6da9249911328ff
akashaf <akashaf93@gmail.com>
2024-10-21 02:21:44 +0800

    feat: Add environment variable for authentication secret
    Added a new environment variable `AUTH_SECRET` to the `.env` file. This variable will be used for authentication purposes.

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,27 @@
-import { Button, Center, Stack } from "@mantine/core";
+import { auth, signOut } from "@/lib/auth";
+import { Button, Center } from "@mantine/core";
+import {redirect} from "next/navigation";
 
-export default function Home() {
+export default async function Home() {
+  const session = await auth()
+  // If user is not authenticated, show built in login page from authjs
+  if (!session) return redirect("/api/auth/signin");
   return (
     <>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti dolore,
+      praesentium perspiciatis obcaecati nulla dolorum alias hic quae explicabo
+      necessitatibus neque expedita recusandae adipisci culpa qui laboriosam
+      commodi quasi? Delectus!
       <Center>
-        <Stack>
-          <span>Hello Auth!</span>
-          <Button>Click Me</Button>
-        </Stack>
+
+      <form
+      action={async () => {
+        "use server"
+        await signOut()
+      }}
+    >
+      <button className="bg-blue-500 rounded px-4 py-2 text-white" type="submit">Sign Out</button>
+    </form>
       </Center>
     </>
   );
